42b91620f7d524640ab8d1eb75efc2abe4bbf1a9
JungSoyi <soi0125@naver.com>
2022-12-27 23:57:43 +0900

    특정 게시글 조회

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,67 +1,67 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private boardsService: BoardsService) { }
+    // constructor(private boardsService: BoardsService) { }
 
-    /**
-     * 모든 게시글을 조회합니다.
-     * @returns 
-     */
-    @Get('/')
-    getAllBoard(): Board[] {
-        return this.boardsService.getAllBoards();
-    }
+    // /**
+    //  * 모든 게시글을 조회합니다.
+    //  * @returns 
+    //  */
+    // @Get('/')
+    // getAllBoard(): Board[] {
+    //     return this.boardsService.getAllBoards();
+    // }
 
-    /**
-     * 게시글을 작성합니다.
-     * @param createBoardDto 
-     * @returns 
-     */
-    @Post()
-    @UsePipes(ValidationPipe)
-    createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    // /**
+    //  * 게시글을 작성합니다.
+    //  * @param createBoardDto 
+    //  * @returns 
+    //  */
+    // @Post()
+    // @UsePipes(ValidationPipe)
+    // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
 
-        return this.boardsService.createBoard(createBoardDto);
+    //     return this.boardsService.createBoard(createBoardDto);
 
-    }
+    // }
 
-    /**
-     * 특정 게시글을 조회합니다.
-     * @param id 
-     * @returns 
-     */
-    @Get('/:id')
-    getBoardById(@Param('id') id: string): Board {
-        return this.boardsService.getBoardById(id);
-    }
+    // /**
+    //  * 특정 게시글을 조회합니다.
+    //  * @param id 
+    //  * @returns 
+    //  */
+    // @Get('/:id')
+    // getBoardById(@Param('id') id: string): Board {
+    //     return this.boardsService.getBoardById(id);
+    // }
 
-    /**
-     * 특정 게시글을 삭제합니다.
-     * @param id 
-     */
-    @Delete('/:id')
-    deleteBoard(@Param('id') id: string): void {
+    // /**
+    //  * 특정 게시글을 삭제합니다.
+    //  * @param id 
+    //  */
+    // @Delete('/:id')
+    // deleteBoard(@Param('id') id: string): void {
 
-        this.boardsService.deleteBoard(id);
+    //     this.boardsService.deleteBoard(id);
 
-    }
+    // }
 
-    /**
-     * 게시글의 상태를 수정합니다.
-     * @param id 
-     * @param status 
-     * @returns 
-     */
-    @Patch('/:id/status')
-    updateBoardStatus(@Param('id') id: string,
-        @Body('status', BoardStatusValidationPipe) status: BoardStatus,): Board {
-        return this.boardsService.updateBoardStatus(id, status);
-    }
+    // /**
+    //  * 게시글의 상태를 수정합니다.
+    //  * @param id 
+    //  * @param status 
+    //  * @returns 
+    //  */
+    // @Patch('/:id/status')
+    // updateBoardStatus(@Param('id') id: string,
+    //     @Body('status', BoardStatusValidationPipe) status: BoardStatus,): Board {
+    //     return this.boardsService.updateBoardStatus(id, status);
+    // }
 
 
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,52 +1,28 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { v1 as uuid } from 'uuid';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
+import { BoardRepository } from './board.repository';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
-    private boards: Board[] = [];
 
+    constructor(
+        @InjectRepository(BoardRepository)
+        private boardRepository: BoardRepository,
+    ) { }
 
-    getAllBoards(): Board[] {
-        return this.boards;
-    }
-
-
-    createBoard(createBoardDto: CreateBoardDto) {
-        const { title, description } = createBoardDto;
-        const board: Board = {
-            id: uuid(),
-            title,
-            description,
-            status: BoardStatus.PUBLIC,
-        };
-        /**
-         * TODO: DB연결 하지 않아 id를 uuid로 사용함. 추후 수정 필요
-         */
-
-        this.boards.push(board);
-        return board;
-    }
-
-    getBoardById(id: string): Board {
-        const found = this.boards.find((board) => board.id === id);
+    async getBoardById(id: number): Promise<Board> {
+        const found = await this.boardRepository.findOne(id);
 
         if (!found) {
-            throw new NotFoundException('Can\'t find Board with id ${id}');
+            throw new NotFoundException('Can\'t find Board with id ${id}')
         }
 
         return found;
     }
 
-    deleteBoard(id: string): void {
-        const found = this.getBoardById(id);
-        this.boards = this.boards.filter((board) => board.id !== found.id);
-    }
 
-    updateBoardStatus(id: string, status: BoardStatus): Board {
-        const board = this.getBoardById(id);
-        board.status = status;
-        return board;
-    }
 }
