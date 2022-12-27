2e24ce530bc90e243824a374bdf3254daa4bc8be
JungSoyi <soi0125@naver.com>
2022-12-27 14:10:01 +0900

    board controller 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [],
+  imports: [BoardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
