6b286b0d24394b15baa8234d6749941f9b475402
JungSoyi <soi0125@naver.com>
2022-12-28 13:10:45 +0900

    user entity 생성

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auto-credential.dto';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        @InjectRepository(User)
+        private userRepository: UserRepository
+    ) { }
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.createUser(authCredentialsDto);
+    }
+}
