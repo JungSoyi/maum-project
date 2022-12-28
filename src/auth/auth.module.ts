0d9a836ceaebdf08a79e98d6f8fb259b4f7f33fa
JungSoyi <soi0125@naver.com>
2022-12-28 14:37:46 +0900

    jwt 모듈 설치

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -6,6 +8,13 @@ import { User } from './user.entity';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt ' }),
+    JwtModule.register({
+      secret: 'Secret1234',
+      signOptions: {
+        expiresIn: 60 * 60,
+      }
+    }),
     TypeOrmModule.forFeature([User])
   ],
   controllers: [AuthController],
